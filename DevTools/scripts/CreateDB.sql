4e5d173cdb69a1936982645223d085b05d32b427
FKEA <fian16ab@student.cbs.dk>
2017-10-10 13:45:58 +0200

    Added DB model file to DevTools folder

---
+++ DevTools/scripts/CreateDB.sql
@@ -0,0 +1,112 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema cafe_nexus
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema cafe_nexus
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cafe_nexus` DEFAULT CHARACTER SET utf8 ;
+USE `cafe_nexus` ;
+
+-- -----------------------------------------------------
+-- Table `cafe_nexus`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cafe_nexus`.`users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `salt` VARCHAR(6) NOT NULL,
+  `password` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(60) NOT NULL,
+  `gender` VARCHAR(1) NOT NULL,
+  `description` LONGTEXT NULL,
+  `major` VARCHAR(60) NULL,
+  `semester` INT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cafe_nexus`.`events`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cafe_nexus`.`events` (
+  `event_id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(60) NOT NULL,
+  `description` LONGTEXT NULL,
+  `created` TIMESTAMP GENERATED ALWAYS AS (CURRENT_TIMESTAMP) VIRTUAL,
+  `start` DATETIME NOT NULL,
+  `end` DATETIME NOT NULL,
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`event_id`),
+  INDEX `fk_events_users1_idx` (`owner_id` ASC),
+  CONSTRAINT `fk_events_users1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `cafe_nexus`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cafe_nexus`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cafe_nexus`.`posts` (
+  `post_id` INT NOT NULL AUTO_INCREMENT,
+  `content` LONGTEXT NOT NULL,
+  `created` TIMESTAMP GENERATED ALWAYS AS (CURRENT_TIMESTAMP) VIRTUAL,
+  `event_id` INT NULL,
+  `parent_id` INT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`post_id`),
+  INDEX `fk_Post_Event_idx` (`event_id` ASC),
+  INDEX `fk_post_post1_idx` (`parent_id` ASC),
+  INDEX `fk_post_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_Post_Event`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `cafe_nexus`.`events` (`event_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_post1`
+    FOREIGN KEY (`parent_id`)
+    REFERENCES `cafe_nexus`.`posts` (`post_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `cafe_nexus`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cafe_nexus`.`events_has_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cafe_nexus`.`events_has_users` (
+  `user_id` INT NOT NULL,
+  `event_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `event_id`),
+  INDEX `fk_user_has_event_event1_idx` (`event_id` ASC),
+  INDEX `fk_user_has_event_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_event_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `cafe_nexus`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_event_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `cafe_nexus`.`events` (`event_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
