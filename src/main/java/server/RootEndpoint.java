70052d196a41613676bcf6a3894499b5015e363d
FKEA <fian16ab@student.cbs.dk>
2017-10-10 14:41:09 +0200

    Added the project structure for the WebServer

---
+++ src/main/java/server/RootEndpoint.java
@@ -0,0 +1,14 @@
+package server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class RootEndpoint {
+
+    @GET
+    public Response defaultGetMethod(){
+        return Response.status(200).type("text/plain").entity("Velkommen til Cafe Nexus").build();
+    }
+}
