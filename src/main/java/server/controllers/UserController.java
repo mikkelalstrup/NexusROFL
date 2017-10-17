2fb20a9edbac97d2eaab0fe4024dd4bea7e428ae
Mikkel Alstrup <mial16ac@student.cbs.dk>
2017-10-17 09:56:12 +0200

    Developing User Creating
    Working in UserEndpoint.java and UserController.java

--- src/main/java/server/controllers/UserController.java
+++ src/main/java/server/controllers/UserController.java
@@ -4,4 +4,32 @@ package server.controllers;
  * Created by Filip on 10-10-2017.
  */
 public class UserController {
+
+    public UserController(){
+
+    }
+    public User validateUserCreation(String password, String firstName, String lastName, String email, char gender, String description, String major, int semester){
+
+        try {
+            User user = new User(password,firstName,lastName,email,gender,description,major,semester);
+
+            String[] emailParts;
+            emailParts = email.split("@");
+            String emailName = emailParts[0];
+            String emailDomain = emailParts[1];
+
+
+            if (!emailDomain.endsWith("cbs.dk") ) {
+                throw new IllegalArgumentException("Email did not contain correct domain");
+            }
+
+        }
+        catch (Exception exception){
+
+            exception.printStackTrace();
+        }
+
+
+        return null;
+    }
 }
--- src/main/java/server/endpoints/UserEndpoint.java
+++ src/main/java/server/endpoints/UserEndpoint.java
@@ -1,15 +1,44 @@
 package server.endpoints;
 
+import server.controllers.UserController;
 import server.models.User;
 import server.providers.UserProvider;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import java.sql.SQLException;
 
 /**
  * Created by Filip on 10-10-2017.
  */
+
+@Path("/users/")
 public class UserEndpoint {
 
+
+    UserController userController = new UserController();
+
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    public Response createUserMethod(
+            @FormParam("password") String password,
+            @FormParam("firstName") String firstName,
+            @FormParam("lastName") String lastName,
+            @FormParam("email") String email,
+            @FormParam("gender") char gender,
+            @FormParam("description") String description,
+            @FormParam("major") String major,
+            @FormParam("semester") int semester){
+
+            userController.validateUserCreation(password, firstName,lastName,email,gender,description,major,semester);
+
+        return Response.status(201).type("text/plain").entity("User created").build();
+
+    }
 
 
 }
