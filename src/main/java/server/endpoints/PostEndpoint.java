67be2396540bd995b4fb692ef5a1847aa428b93c
FKEA <fian16ab@student.cbs.dk>
2017-10-17 19:16:18 +0200

    Added ability to fetch comments from a post

--- src/main/java/server/endpoints/PostEndpoint.java
+++ src/main/java/server/endpoints/PostEndpoint.java
@@ -74,10 +74,12 @@ public class PostEndpoint {
      */
     @GET
     @Path("{id}")
-    public Response getPost(@PathParam("id") int id) {
+    public Response getPost(@PathParam("id") int post_id) {
         PostProvider postProvider = new PostProvider(); //Creates an object
 
-        Post post = postProvider.getPost(id);
+        Post post = postProvider.getPost(post_id);
+
+        post.getComments().addAll(postProvider.getPostsByParentId(post_id));
 
         return Response.status(200).type("application/json").entity(new Gson().toJson(post)).build();
 
