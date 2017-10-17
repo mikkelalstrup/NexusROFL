c13d892215af2f6da56bed53312084610c35c663
Mikkel Alstrup <mial16ac@student.cbs.dk>
2017-10-17 10:37:16 +0200

    Fixes for user creation
    Added validation for usercreation.

--- src/main/java/server/controllers/UserController.java
+++ src/main/java/server/controllers/UserController.java
@@ -1,5 +1,7 @@
 package server.controllers;
 
+import server.models.User;
+
 /**
  * Created by Filip on 10-10-2017.
  */
@@ -10,8 +12,9 @@ public class UserController {
     }
     public User validateUserCreation(String password, String firstName, String lastName, String email, char gender, String description, String major, int semester){
 
+        User validatedUser = new User(password,firstName,lastName,email,gender,description,major,semester);
+
         try {
-            User user = new User(password,firstName,lastName,email,gender,description,major,semester);
 
             String[] emailParts;
             emailParts = email.split("@");
@@ -19,17 +22,22 @@ public class UserController {
             String emailDomain = emailParts[1];
 
 
-            if (!emailDomain.endsWith("cbs.dk") ) {
-                throw new IllegalArgumentException("Email did not contain correct domain");
+            if (!emailDomain.endsWith("cbs.dk") && emailDomain.length()!=6 || !emailDomain.endsWith(".cbs.dk")) {
+                throw new IllegalArgumentException("Email did not contain correct a domain");
+            }
+            else if (!String.valueOf(gender).equalsIgnoreCase("f")
+                    || !String.valueOf(gender).equalsIgnoreCase("m")) {
+                throw new IllegalArgumentException("Gender can only be \"m\" or \"f\"");
+            } else if (semester <1 || semester >6) {
+                throw new IllegalArgumentException("Semester must be a value of 1-6");
             }
 
         }
         catch (Exception exception){
-
             exception.printStackTrace();
         }
 
 
-        return null;
+        return validatedUser;
     }
 }
