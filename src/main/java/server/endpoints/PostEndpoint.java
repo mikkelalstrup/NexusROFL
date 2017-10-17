b8179cd58150aa1036918912cd9a1134548ebe4b
merimais <meis16ab@student.cbs.dk>
2017-10-17 10:36:36 +0200

    Started working on postmethod in PostProvider

--- src/main/java/server/endpoints/PostEndpoint.java
+++ src/main/java/server/endpoints/PostEndpoint.java
@@ -1,7 +1,23 @@
 package server.endpoints;
 
+import server.util.DBManager;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.xml.ws.Response;
+import java.sql.SQLException;
+
 /**
  * Created by Filip on 10-10-2017.
  */
+@Path("/posts")
 public class PostEndpoint {
+
+    @POST
+    public Response createPost() {
+
+
+        }
+    }
+
 }
