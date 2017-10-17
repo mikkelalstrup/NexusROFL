c0f47d8833931b9a318e3ecd5ec3ef622a054ddc
Oscar Krarup <oskr16ab@student.cbs.dk>
2017-10-17 16:11:01 +0200

    Get event by id
    uden kommentarer

--- src/main/java/server/endpoints/EventEndpoint.java
+++ src/main/java/server/endpoints/EventEndpoint.java
@@ -1,7 +1,30 @@
 package server.endpoints;
 
+import com.google.gson.Gson;
+import server.controllers.ContentController;
+import server.models.Event;
+import server.providers.EventProvider;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
 /**
  * Created by Filip on 10-10-2017.
  */
+
+@Path("/events")
 public class EventEndpoint {
+
+    @GET
+    @Path("{id}")
+    public Response getEvent(@PathParam("id") int event_id){
+        EventProvider eventProvider = new EventProvider();
+        Event event = eventProvider.getEvent(event_id);
+        
+        return Response.status(200).type("application/json").entity(new Gson().toJson(event)).build();
+
+    }
 }
