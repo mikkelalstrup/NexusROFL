773e5e182c97143cdde36c3fe4a11ea13de5a8bf
merimais <meis16ab@student.cbs.dk>
2017-10-17 12:33:45 +0200

    Created PostEndpoint

--- src/main/java/server/endpoints/PostEndpoint.java
+++ src/main/java/server/endpoints/PostEndpoint.java
@@ -1,11 +1,16 @@
 package server.endpoints;
 
+import server.models.Post;
+import server.models.User;
+import server.providers.PostProvider;
 import server.util.DBManager;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.xml.ws.Response;
-import java.sql.SQLException;
+import javax.ws.rs.core.Response;
+import java.sql.*;
 
 /**
  * Created by Filip on 10-10-2017.
@@ -14,10 +19,32 @@ import java.sql.SQLException;
 public class PostEndpoint {
 
     @POST
-    public Response createPost() {
+    @Consumes("application/x-www-form-urlencoded")
+    public Response createPostMethod(
+            @FormParam("owner") int owner,
+            @FormParam("content") String content,
+            @FormParam("event") int event,
+            @FormParam("parent") int parent) {
+
+        //Creating instance of PostProvider
+        PostProvider postProvider = new PostProvider();
+
+        //Creating Post object
+        Post post = new Post(owner, content, event, parent);
+
+        //Klad metode i Post pro
+        try {
+            postProvider.createPost(post);
+
+            return Response.status(201).type("").entity("Post created").build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response.status(401).build();
 
 
         }
-    }
 
-}
+
+        }
+
+    }
